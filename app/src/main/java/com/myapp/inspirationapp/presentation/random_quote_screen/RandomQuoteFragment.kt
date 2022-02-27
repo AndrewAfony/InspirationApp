acda709b6259ae51303e76582b9feb9ed9c0eabc
Andrew <andrew.afony@yandex.ru>
2022-02-27 13:53:47 +0300

    add getting random quote

--- app/src/main/java/com/myapp/inspirationapp/presentation/random_quote_screen/RandomQuoteFragment.kt
+++ app/src/main/java/com/myapp/inspirationapp/presentation/random_quote_screen/RandomQuoteFragment.kt
@@ -5,16 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.myapp.inspirationapp.R
+import com.myapp.inspirationapp.databinding.FragmentRandomQuoteBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RandomQuoteFragment : Fragment() {
+
+    private val viewModel: RandomQuoteViewModel by viewModels()
+
+    private var _binding: FragmentRandomQuoteBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_random_quote, container, false)
+    ): View {
+        _binding = FragmentRandomQuoteBinding.inflate(inflater, container, false)
+
+        viewModel.getQuote()
+
+        viewModel.randomQuote.observe(viewLifecycleOwner, {
+            binding.quote.text = it.content
+            binding.author.text = it.author
+        })
+
+
+        return binding.root
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
--- app/src/main/java/com/myapp/inspirationapp/presentation/random_quote_screen/RandomQuoteViewModel.kt
+++ app/src/main/java/com/myapp/inspirationapp/presentation/random_quote_screen/RandomQuoteViewModel.kt
@@ -1,8 +1,13 @@
 package com.myapp.inspirationapp.presentation.random_quote_screen
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.myapp.inspirationapp.domain.model.RandomQuote
 import com.myapp.inspirationapp.domain.repository.QuoteRepository
+import com.myapp.inspirationapp.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,5 +15,23 @@ class RandomQuoteViewModel @Inject constructor(
     private val repository: QuoteRepository
 ): ViewModel() {
 
+    var randomQuote = MutableLiveData<RandomQuote>()
+    private set
+
+    fun getQuote() {
+        viewModelScope.launch {
+            val result = repository.getRandomQuote()
+
+            when (result) {
+                is Resource.Success -> {
+                    randomQuote.postValue(result.data!!)
+                }
+                is Resource.Error -> {
+                    randomQuote.postValue(result.data!!)
+                }
+                else -> {}
+            }
+        }
+    }
 
 }
