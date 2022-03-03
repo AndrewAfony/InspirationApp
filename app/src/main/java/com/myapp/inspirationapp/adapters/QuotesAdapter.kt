3ebe8d822b1c650d6c8a9f618f9d3bd01bcf7083
Andrew <andrew.afony@yandex.ru>
2022-03-03 11:13:55 +0300

    add popup menu on click on quote

--- app/src/main/java/com/myapp/inspirationapp/adapters/QuotesAdapter.kt
+++ app/src/main/java/com/myapp/inspirationapp/adapters/QuotesAdapter.kt
@@ -1,10 +1,16 @@
 package com.myapp.inspirationapp.adapters
 
+import android.content.Context
+import android.os.Build
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.myapp.inspirationapp.R
 import com.myapp.inspirationapp.databinding.ItemQuoteBinding
 import com.myapp.inspirationapp.domain.model.Quote
 import com.myapp.inspirationapp.utils.toCategory
@@ -38,10 +44,42 @@ class QuotesAdapter: RecyclerView.Adapter<QuotesAdapter.QuoteViewHolder>() {
             quoteText.text = quote.content
             author.text = quote.author
             tag.text = quote.tags[0].toCategory()
+
+            root.setOnLongClickListener {
+                showMenu(this.root.context, it)
+                true
+            }
         }
     }
 
     override fun getItemCount(): Int {
         return differ.currentList.size
+    }
+}
+
+private fun showMenu(context: Context, view: View) {
+
+    PopupMenu(context, view).apply {
+
+        setOnMenuItemClickListener {
+            return@setOnMenuItemClickListener when (it.itemId) {
+                R.id.save_to_favorite -> {
+                    Toast.makeText(context, "Saved", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.share -> {
+                    true
+                }
+                else -> false
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            setForceShowIcon(true)
+        }
+
+        inflate(R.menu.quote_menu)
+        show()
+
     }
 }
