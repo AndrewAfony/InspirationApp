8ed278efaf73f9e246b1bc4f02d1cae6b1afa135
Andrew <andrew.afony@yandex.ru>
2022-03-04 18:36:33 +0300

    add recycler view to search fragment

--- app/src/main/java/com/myapp/inspirationapp/presentation/SearchQuoteFragment.kt
+++ app/src/main/java/com/myapp/inspirationapp/presentation/SearchQuoteFragment.kt
@@ -6,15 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.myapp.inspirationapp.R
+import com.myapp.inspirationapp.databinding.FragmentSearchQuoteBinding
 
 class SearchQuoteFragment : Fragment() {
+
+    private var _binding: FragmentSearchQuoteBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_quote, container, false)
+    ): View {
+
+        _binding = FragmentSearchQuoteBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
--- app/src/main/java/com/myapp/inspirationapp/utils/StringUtils.kt
+++ app/src/main/java/com/myapp/inspirationapp/utils/StringUtils.kt
@@ -4,7 +4,7 @@ import java.util.*
 
 fun String.toCategory() : String {
 
-    val result = this.uppercase(Locale.getDefault())
+    val result = this.replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
 
     if (result.contains("-")) {
         val first = result.substringBefore("")
