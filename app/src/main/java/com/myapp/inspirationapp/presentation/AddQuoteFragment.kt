f4ce681b52edab3c66b0915a0557841145d8b977
Andrew <andrew.afony@yandex.ru>
2022-03-09 19:43:38 +0300

    add creating custom quote

--- app/src/main/java/com/myapp/inspirationapp/presentation/AddQuoteFragment.kt
+++ app/src/main/java/com/myapp/inspirationapp/presentation/AddQuoteFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.children
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.chip.Chip
+import com.google.android.material.snackbar.Snackbar
 import com.myapp.inspirationapp.R
 import com.myapp.inspirationapp.databinding.FragmentAddQuoteBinding
+import com.myapp.inspirationapp.domain.model.Quote
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
 @AndroidEntryPoint
 class AddQuoteFragment : Fragment() {
@@ -18,6 +24,8 @@ class AddQuoteFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var bottomNav: BottomNavigationView
+
+    private val viewModel: QuotesViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,6 +46,46 @@ class AddQuoteFragment : Fragment() {
         binding.toolbar.setNavigationOnClickListener {
             view.findNavController().navigateUp()
             bottomNav.visibility = View.VISIBLE
+        }
+
+        binding.buttonCreateQuote.setOnClickListener {
+
+            var quoteText: String = ""
+            var author: String = ""
+            val tag: MutableList<String> = mutableListOf()
+
+            if(binding.customQuote.text.toString().isBlank()) {
+                binding.customQuote.error = "Can't be empty!"
+            } else {
+                quoteText = binding.customQuote.text.toString()
+            }
+
+            if(binding.customAuthor.text.toString().isBlank()){
+                binding.customAuthor.error = "Can't be empty!"
+            } else {
+                author = binding.customAuthor.text.toString()
+            }
+
+            val selected = view.findViewById<Chip>(binding.chipGroup.checkedChipId)
+
+            if(selected == null) {
+                Snackbar.make(view, "Select tag", Snackbar.LENGTH_SHORT).show()
+            } else {
+                tag.add(selected.text.toString())
+            }
+
+            val quote = Quote(
+                _id = UUID.randomUUID().toString(),
+                author = author,
+                content = quoteText,
+                tags = tag
+            )
+
+            viewModel.saveQuote(quote)
+            binding.customQuote.text.clear()
+            binding.customAuthor.text.clear()
+            binding.chipGroup.clearCheck()
+            Snackbar.make(view, "Quote added", Snackbar.LENGTH_SHORT).show()
         }
     }
 
