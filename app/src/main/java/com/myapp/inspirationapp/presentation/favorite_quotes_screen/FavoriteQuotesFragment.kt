22989cfb7592a91fe7f306e2741d676bcdb8d3e4
Andrew <andrew.afony@yandex.ru>
2022-03-25 11:59:15 +0300

    change app icon

--- app/src/main/java/com/myapp/inspirationapp/presentation/favorite_quotes_screen/FavoriteQuotesFragment.kt
+++ app/src/main/java/com/myapp/inspirationapp/presentation/favorite_quotes_screen/FavoriteQuotesFragment.kt
@@ -1,11 +1,9 @@
 package com.myapp.inspirationapp.presentation.favorite_quotes_screen
 
-import android.app.ProgressDialog.show
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
